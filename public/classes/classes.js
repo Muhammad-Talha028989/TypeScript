42246cac6c527278b59cbd52b95847a2c62133b9
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-07-17 22:42:04 +0500

    add abstract classes

--- public/classes/classes.js
+++ public/classes/classes.js
@@ -1,19 +1,19 @@
-class NodeTree {
-    constructor(value, link) {
-        this.value = 0;
-        this.nextLink = null;
-        this.NodeTree = (Value, link) => {
-            this.value = Value;
-            this.nextLink = link;
+class Car {
+    constructor() {
+        this.printCar = () => {
+            console.log("Best car is " + this.getCarName());
         };
-        this.printNodeTree = () => {
-            var _a;
-            console.log([this.value, (_a = this.nextLink) === null || _a === void 0 ? void 0 : _a.value]);
-        };
-        this.value = value;
-        this.nextLink = link;
     }
 }
-const node = new NodeTree(1, new NodeTree(2, null));
-node.printNodeTree();
+class Honda extends Car {
+    constructor(carName) {
+        super();
+        this.hondaCarName = carName;
+    }
+    getCarName() {
+        return this.hondaCarName;
+    }
+}
+let honda = new Honda("Honda corol");
+honda.printCar();
 //# sourceMappingURL=classes.js.map
--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -1,5 +1,29 @@
+//? Abstract classes using typescript.
+
+abstract class Car {
+  abstract getCarName(): string;
+  printCar = (): void => {
+    console.log("Best car is " + this.getCarName());
+  };
+}
+
+
+class Honda extends Car {
+  hondaCarName: string;
+  constructor(carName: string) {
+    super();
+    this.hondaCarName = carName;
+  }
+  getCarName(): string {
+    return this.hondaCarName;
+  }
+}
+
+let honda = new Honda("Honda corol");
+honda.printCar();
+
 //? Basic of classes in Typescript.
-class NodeTree {
+/*class NodeTree {
   value: number = 0;
     nextLink: NodeTree = null;
 
@@ -25,3 +49,5 @@ class NodeTree {
 const node = new NodeTree(1,new NodeTree(2,null)); //? called the defualt constructer....
 // node.NodeTree( 1, null );
 node.printNodeTree()
+
+//?*/
