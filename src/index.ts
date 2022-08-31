3324404f705f17a1f6692da9c45d4b59e2e692a0
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-08-31 10:57:52 +0500

    evaluating the process time start and end

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,42 @@
-/**
- * Typescript Utility Types
- */
+// /**
+//  * Typescript Utility Types
+//  */
 
-// Partial types
-interface student {
-  name: string;
-  rollno: string;
+// // Partial types
+// interface student {
+//   name: string;
+//   rollno: string;
+// }
+
+// const getStudent = (Students: student, optional: Partial<student>) => ({
+//   ...Students,
+//   ...optional,
+// });
+
+// let st: student = {
+//   name: "Muhammad Talha",
+//   rollno: "112/028989",
+// };
+
+// const st2 = getStudent(st, {});
+
+// console.info(st2);
+let _startProcessTime: number = Number(new Date().getTime().toFixed(3));
+
+console.info(`Process Start ${0} ms`);
+
+for (let index = 0; index < 15; index++) {
+  let sum: number = index + 1;
+  let _ProcessTimeEnd: number = Number(new Date().getTime().toFixed(3));
+
+  let _overlapsTime: number = parseFloat(
+    String(_ProcessTimeEnd / _startProcessTime),
+  );
+  console.info(`overtime ${_overlapsTime} ms`);
 }
 
-const getStudent = (Students: student, optional: Partial<student>) => ({
-  ...Students,
-  ...optional,
-});
+let _endProcessTime: number = Number(new Date().getTime().toFixed(3));
 
-let st: student = {
-  name: "Muhammad Talha",
-  rollno: "112/028989",
-};
+let _overAllTime: number = _endProcessTime - _startProcessTime;
 
-const st2 = getStudent(st,{});
-
-console.info(st2);
+console.info(`Process End ${_overAllTime} ms`);
